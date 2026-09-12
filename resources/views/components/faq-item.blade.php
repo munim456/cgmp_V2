532c466a9478864ecb93794c9ab0d4ca769c3d44
Munim <muhtasimhaque2@gmail.com>
2026-09-13 02:18:34 +0600

    Match FAQ answer text size to the question text size

--- resources/views/components/faq-item.blade.php
+++ resources/views/components/faq-item.blade.php
@@ -7,5 +7,5 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 12 15 18 9"/></svg>
         </span>
     </summary>
-    <p style="{{ text_style($styles, 'answer') }}" class="border-t border-[#E5E7EB] px-6 pb-6 pt-5 leading-7 text-[#6B7280]">{{ $answer }}</p>
+    <p style="{{ text_style($styles, 'answer') }}" class="border-t border-[#E5E7EB] px-6 pb-6 pt-5 text-[3.3vw] leading-7 text-[#6B7280] sm:text-[17px]">{{ $answer }}</p>
 </details>
