3afcb22bec3a3d3bbf9d3183448cd27823a61c75
Munim <muhtasimhaque2@gmail.com>
2026-09-12 14:07:58 +0600

    Keep logo name and tagline each on a single line, no width cap

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -9,8 +9,8 @@
             <div class="absolute left-6 top-1 h-9 w-16 -rotate-[30deg] rounded-[100%] border-t-[7px] border-brand-green"></div>
         </div>
     @endif
-    <div class="max-w-[120px] text-center font-serif text-[10px] leading-[1.4]">
-        <div>{{ strtoupper(setting('clinic_name', 'Cringila General Medical Practice')) }}</div>
-        <div class="mt-1 font-sans text-[6.5px] tracking-[.24em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
+    <div class="text-center font-serif text-[10px] leading-[1.4]">
+        <div class="whitespace-nowrap">{{ strtoupper(setting('clinic_name', 'Cringila General Medical Practice')) }}</div>
+        <div class="mt-1 whitespace-nowrap font-sans text-[6.5px] tracking-[.24em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
     </div>
 </div>
