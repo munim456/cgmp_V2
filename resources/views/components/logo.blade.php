2097524c8a8efb729c3f7471048f3de525ef83e2
Munim <muhtasimhaque2@gmail.com>
2026-09-12 13:51:21 +0600

    Contain swoosh icon within its box and shrink logo text further

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -4,11 +4,11 @@
     @if(setting('logo_path'))
         <img src="{{ image_url(setting('logo_path')) }}" alt="{{ setting('clinic_name', 'Clinic logo') }}" class="h-6 w-full object-contain">
     @else
-        <div class="relative h-6 w-full">
+        <div class="relative h-6 w-full overflow-hidden">
             <div class="absolute inset-x-2 top-1 h-1.5 -rotate-12 rounded-[100%] border-t-[4px] border-brand-blue"></div>
             <div class="absolute inset-x-4 top-0 h-5 -rotate-[30deg] rounded-[100%] border-t-[4px] border-brand-green"></div>
         </div>
     @endif
-    <div class="mt-0.5 whitespace-nowrap font-serif text-[10px] font-bold uppercase tracking-wider">{{ setting('clinic_name', 'Cringila General Medical Practice') }}</div>
-    <div class="mt-0.5 whitespace-nowrap font-sans text-[6px] font-semibold tracking-[.3em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
+    <div class="mt-1 whitespace-nowrap font-serif text-[9px] font-bold uppercase tracking-wide">{{ setting('clinic_name', 'Cringila General Medical Practice') }}</div>
+    <div class="mt-0.5 whitespace-nowrap font-sans text-[5px] font-semibold tracking-[.25em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
 </div>
