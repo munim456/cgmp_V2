f71b9d066b770e10aa1b24860074db67b6e80455
Munim <muhtasimhaque2@gmail.com>
2026-09-12 13:32:11 +0600

    Restyle logo as a centered banner: swoosh on top, name and tagline stacked below

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,16 +1,14 @@
 @props(['light' => false])
 
-<div {{ $attributes->merge(['class' => 'flex items-center gap-2 ' . ($light ? 'text-white' : 'text-brand-blue')]) }}>
+<div {{ $attributes->merge(['class' => 'inline-flex flex-col items-center text-center ' . ($light ? 'text-white' : 'text-brand-blue')]) }}>
     @if(setting('logo_path'))
-        <img src="{{ image_url(setting('logo_path')) }}" alt="{{ setting('clinic_name', 'Clinic logo') }}" class="h-14 w-24 shrink-0 object-contain">
+        <img src="{{ image_url(setting('logo_path')) }}" alt="{{ setting('clinic_name', 'Clinic logo') }}" class="h-14 w-full object-contain">
     @else
-        <div class="relative h-14 w-24 shrink-0">
-            <div class="absolute left-2 top-3 h-3 w-20 -rotate-12 rounded-[100%] border-t-[7px] border-brand-blue"></div>
-            <div class="absolute left-7 top-1 h-12 w-20 -rotate-[30deg] rounded-[100%] border-t-[8px] border-brand-green"></div>
+        <div class="relative h-14 w-full">
+            <div class="absolute inset-x-2 top-3 h-3 -rotate-12 rounded-[100%] border-t-[7px] border-brand-blue"></div>
+            <div class="absolute inset-x-4 top-1 h-12 -rotate-[30deg] rounded-[100%] border-t-[8px] border-brand-green"></div>
         </div>
     @endif
-    <div class="max-w-[130px] text-left font-serif text-[11px] leading-[1.5]">
-        <div>{{ strtoupper(setting('clinic_name', 'Cringila General Medical Practice')) }}</div>
-        <div class="mt-1.5 font-sans text-[7px] tracking-[.28em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
-    </div>
+    <div class="mt-1.5 whitespace-nowrap font-serif text-sm font-bold uppercase tracking-wider">{{ setting('clinic_name', 'Cringila General Medical Practice') }}</div>
+    <div class="mt-1 whitespace-nowrap font-sans text-[8px] font-semibold tracking-[.3em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
 </div>
