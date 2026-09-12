446562f50883cfd90dd08fb0fe86d39311185e6a
Munim <muhtasimhaque2@gmail.com>
2026-09-12 14:03:19 +0600

    Move logo text below the icon, keep the icon itself unchanged

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,6 +1,6 @@
 @props(['light' => false])
 
-<div {{ $attributes->merge(['class' => 'flex items-center gap-2 ' . ($light ? 'text-white' : 'text-brand-blue')]) }}>
+<div {{ $attributes->merge(['class' => 'flex flex-col items-center gap-1 ' . ($light ? 'text-white' : 'text-brand-blue')]) }}>
     @if(setting('logo_path'))
         <img src="{{ image_url(setting('logo_path')) }}" alt="{{ setting('clinic_name', 'Clinic logo') }}" class="h-11 w-20 shrink-0 object-contain">
     @else
@@ -9,7 +9,7 @@
             <div class="absolute left-6 top-1 h-9 w-16 -rotate-[30deg] rounded-[100%] border-t-[7px] border-brand-green"></div>
         </div>
     @endif
-    <div class="max-w-[120px] text-left font-serif text-[10px] leading-[1.4]">
+    <div class="max-w-[120px] text-center font-serif text-[10px] leading-[1.4]">
         <div>{{ strtoupper(setting('clinic_name', 'Cringila General Medical Practice')) }}</div>
         <div class="mt-1 font-sans text-[6.5px] tracking-[.24em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
     </div>
