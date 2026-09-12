a945c976a4438cd9f12f4c46e15fdac1a997953b
Munim <muhtasimhaque2@gmail.com>
2026-09-12 14:33:55 +0600

    Increase mobile hamburger icon to a more moderate size

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -28,9 +28,9 @@
 
             <x-healthengine-button :label="$bookIcon . 'Book Appointment'" class="btn-lift hidden items-center gap-2 rounded-xl bg-brand-blue px-4 py-2.5 text-sm font-bold text-white shadow-md hover:bg-brand-blue-dark hover:shadow-lg md:flex" />
 
-            <button aria-label="Open menu" class="lg:hidden" @click="open = !open">
-                <svg x-show="!open" x-transition.opacity.duration.150ms xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 6h16M4 12h16M4 18h16"/></svg>
-                <svg x-show="open" x-transition.opacity.duration.150ms xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18M6 6l12 12"/></svg>
+            <button aria-label="Open menu" class="p-1 lg:hidden" @click="open = !open">
+                <svg x-show="!open" x-transition.opacity.duration.150ms xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 6h16M4 12h16M4 18h16"/></svg>
+                <svg x-show="open" x-transition.opacity.duration.150ms xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18M6 6l12 12"/></svg>
             </button>
         </div>
 
