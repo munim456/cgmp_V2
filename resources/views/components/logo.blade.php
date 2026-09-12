8a1af3182fa5ad8ab7862cdff0da932855b4758d
Munim <muhtasimhaque2@gmail.com>
2026-09-12 13:59:47 +0600

    Stack logo icon above one-line practice name, give header slightly more room

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,16 +1,14 @@
 @props(['light' => false])
 
-<div {{ $attributes->merge(['class' => 'flex items-center gap-2 ' . ($light ? 'text-white' : 'text-brand-blue')]) }}>
+<div {{ $attributes->merge(['class' => 'inline-flex flex-col items-center text-center ' . ($light ? 'text-white' : 'text-brand-blue')]) }}>
     @if(setting('logo_path'))
-        <img src="{{ image_url(setting('logo_path')) }}" alt="{{ setting('clinic_name', 'Clinic logo') }}" class="h-11 w-20 shrink-0 object-contain">
+        <img src="{{ image_url(setting('logo_path')) }}" alt="{{ setting('clinic_name', 'Clinic logo') }}" class="h-9 w-full object-contain">
     @else
-        <div class="relative h-11 w-20 shrink-0">
-            <div class="absolute left-1.5 top-2.5 h-2.5 w-16 -rotate-12 rounded-[100%] border-t-[6px] border-brand-blue"></div>
-            <div class="absolute left-6 top-1 h-9 w-16 -rotate-[30deg] rounded-[100%] border-t-[7px] border-brand-green"></div>
+        <div class="relative h-9 w-full overflow-hidden">
+            <div class="absolute inset-x-2 top-1.5 h-2 -rotate-12 rounded-[100%] border-t-[5px] border-brand-blue"></div>
+            <div class="absolute inset-x-4 top-0.5 h-7 -rotate-[30deg] rounded-[100%] border-t-[5px] border-brand-green"></div>
         </div>
     @endif
-    <div class="max-w-[120px] text-left font-serif text-[10px] leading-[1.4]">
-        <div>{{ strtoupper(setting('clinic_name', 'Cringila General Medical Practice')) }}</div>
-        <div class="mt-1 font-sans text-[6.5px] tracking-[.24em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
-    </div>
+    <div class="mt-1.5 whitespace-nowrap font-serif text-[11px] font-bold uppercase tracking-wide">{{ setting('clinic_name', 'Cringila General Medical Practice') }}</div>
+    <div class="mt-1 whitespace-nowrap font-sans text-[7px] font-semibold tracking-[.28em] text-brand-green">{{ strtoupper(setting('tagline', 'Healthcare for Every Generation')) }}</div>
 </div>
